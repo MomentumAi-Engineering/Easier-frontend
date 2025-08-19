b0d1a6d67f9be3e73beff21e70e43b999d90d3ca
Chrishabh2002 <chrishabh100@gmail.com>
2025-08-19 14:35:59 +0530

    update the frontend

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,5 @@
 const API_BASE_URL = import.meta.env.MODE === 'development'
   ? 'http://localhost:8000/api'
-  : 'https://eaiser-backend.onrender.com';
+  : 'https://eaiser-backend.onrender.com/issues';
 
 export default API_BASE_URL;
